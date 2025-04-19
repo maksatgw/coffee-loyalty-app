97dacb09c9c470a6d039cd6cce5dfd1ee6c044ec
Maksat Oğuzhan Garyagdyyev <garyagdyyev@outlook.com>
2025-04-19 21:37:03 +0300

    Add Customer model with basic properties and QR code support

---
+++ CoffeLoyaltyApp/Models/Customer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace CoffeeLoyaltyApp.Models
+{
+    public class Customer
+    {
+        [Key]
+        public Guid CustomerId { get; set; } = Guid.NewGuid();
+
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; }
+
+        [EmailAddress]
+        public string? Email { get; set; }
+
+        [Phone]
+        public string? Phone { get; set; }
+
+        [Required]
+        public string QRCode { get; set; } = Guid.NewGuid().ToString();
+
+        // Navigation Property
+        public ICollection<CoffeePurchase>? CoffeePurchases { get; set; }
+    }
+}
