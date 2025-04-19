a4914ede962fab3118cb778cc1e5cb47db762774
Maksat Oğuzhan Garyagdyyev <garyagdyyev@outlook.com>
2025-04-19 21:45:57 +0300

    Add AppDbContext with DbSets and entity relationships

---
+++ CoffeLoyaltyApp/Data/AppDbContext.cs
@@ -0,0 +1,33 @@
+﻿using CoffeeLoyaltyApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CoffeeLoyaltyApp.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<MenuItem> MenuItems { get; set; }
+        public DbSet<CoffeePurchase> CoffeePurchases { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Opsiyonel: İlişki ayarları (EF genelde otomatik tanır ama senin için örnek):
+            modelBuilder.Entity<CoffeePurchase>()
+                .HasOne(p => p.Customer)
+                .WithMany(c => c.CoffeePurchases)
+                .HasForeignKey(p => p.CustomerId);
+
+            modelBuilder.Entity<CoffeePurchase>()
+                .HasOne(p => p.MenuItem)
+                .WithMany(m => m.CoffeePurchases)
+                .HasForeignKey(p => p.MenuItemId);
+        }
+    }
+}
--- CoffeLoyaltyApp/obj/Debug/net7.0/CoffeLoyaltyApp.AssemblyInfo.cs
+++ CoffeLoyaltyApp/obj/Debug/net7.0/CoffeLoyaltyApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CoffeLoyaltyApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05091929c13a6944e955269a32d140278d5ee74c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d25253a4936f94c92dff06db65983256876e5e9")]
 [assembly: System.Reflection.AssemblyProductAttribute("CoffeLoyaltyApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CoffeLoyaltyApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
