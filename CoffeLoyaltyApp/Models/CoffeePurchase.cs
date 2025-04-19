3d25253a4936f94c92dff06db65983256876e5e9
Maksat Oğuzhan Garyagdyyev <garyagdyyev@outlook.com>
2025-04-19 21:40:04 +0300

    Add CoffeePurchase model to track customer purchases and free coffee logic

---
+++ CoffeLoyaltyApp/Models/CoffeePurchase.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CoffeeLoyaltyApp.Models
+{
+    public class CoffeePurchase
+    {
+        [Key]
+        public Guid PurchaseId { get; set; } = Guid.NewGuid();
+
+        [Required]
+        public Guid CustomerId { get; set; }
+
+        [Required]
+        public Guid MenuItemId { get; set; }
+
+        public DateTime PurchaseDate { get; set; } = DateTime.UtcNow;
+
+        public bool IsFree { get; set; } = false;
+
+        // Navigation Properties
+        public Customer? Customer { get; set; }
+        public MenuItem? MenuItem { get; set; }
+    }
+}
