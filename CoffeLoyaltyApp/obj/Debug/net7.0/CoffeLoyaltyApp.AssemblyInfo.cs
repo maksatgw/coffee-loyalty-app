d7ccfdd8943e652eb2d0eb8322525fe2f069f2be
Maksat Oğuzhan Garyagdyyev <garyagdyyev@outlook.com>
2025-04-20 13:51:41 +0300

    Add User entity with 1:1 relation to Customer

--- CoffeLoyaltyApp/obj/Debug/net7.0/CoffeLoyaltyApp.AssemblyInfo.cs
+++ CoffeLoyaltyApp/obj/Debug/net7.0/CoffeLoyaltyApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CoffeLoyaltyApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dfa810c43fbf2303b295c6fe4b1e20591837d555")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d9980872b6877651326ff8ab6d8ec44e2dad99cc")]
 [assembly: System.Reflection.AssemblyProductAttribute("CoffeLoyaltyApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CoffeLoyaltyApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
