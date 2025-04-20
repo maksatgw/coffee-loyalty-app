23adb6eece1a9980798ffd6c7bbdd3f89529426a
Maksat Oğuzhan Garyagdyyev <garyagdyyev@outlook.com>
2025-04-20 13:53:34 +0300

    Add RegisterDto for combined user and customer registration

--- CoffeLoyaltyApp/DTOs/ScanPurchaseDto.cs
+++
@@ -1,7 +0,0 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-public class ScanPurchaseDto
-{
-    [Required] public string QrCode { get; set; }
-    [Required] public Guid MenuItemId { get; set; }
-}
--- CoffeLoyaltyApp/obj/Debug/net7.0/CoffeLoyaltyApp.AssemblyInfo.cs
+++ CoffeLoyaltyApp/obj/Debug/net7.0/CoffeLoyaltyApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CoffeLoyaltyApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d9980872b6877651326ff8ab6d8ec44e2dad99cc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7ccfdd8943e652eb2d0eb8322525fe2f069f2be")]
 [assembly: System.Reflection.AssemblyProductAttribute("CoffeLoyaltyApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CoffeLoyaltyApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
